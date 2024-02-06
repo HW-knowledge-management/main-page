e4ed62ac6541a1bf0a68a9e3e54f62741ecbb504
minu1215 <mkw3295@gmail.com>
2024-02-06 17:13:01 +0900

    login

---
+++ vuetify-project.backup/router/router.js
@@ -0,0 +1,25 @@
+//📁router/router.js
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Main from '../views/Main.vue';
+import Info from '../views/Info.vue';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history', //해쉬값 제거 방식
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      component: Main
+    },
+    {
+      path: '/Info',
+      component: Info
+    }
+  ]
+});
---
+++ vuetify-project.backup/src/components/sign-up.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { ref, defineEmits } from 'vue'
+import Cookies from 'js-cookie'
+
+const emit = defineEmits(['changeComponent'])
+
+const username = ref('')
+const password = ref('')
+const userData = ref(null)
+
+async function submitForm() {
+
+
+    
+    const res = await fetch(
+        `http://localhost:3001/User`
+    )
+  
+    userData.value = await res.json()
+
+    const result = userData.value.find(item => item.userId === username.value)
+
+    console.log(userData.value.length)
+
+    if(result){
+        alert('중복되는 ID입니다.')
+        username.value = ''
+    } else{
+        const myData = {
+            "username" : username.value,
+            "password" : password.value,
+            "id" : userData.value.length + 1 + ''
+            }
+        await fetch('http://localhost:3001/User', {
+            method: 'POST', // 또는 'PUT', 'GET'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(myData),
+ 
+        })
+
+        Cookies.set('username', username.value, { expires: 1, path: '' })
+        Cookies.set('password', password.value, { expires: 1, path: '' })
+
+        emit('changeComponent');
+
+    }
+
+//  emit('changeComponent');
+}
+
+</script>
+
+<template>
+<h1>sign-up</h1>
+<form @submit.prevent="submitForm()">
+    <label>id</label>
+    <input id="username" type="text" v-model="username">
+
+    <label>password</label>
+    <input id="password" type="password" v-model="password">
+    <input type="submit" value="확인"></input>
+</form>
+</template>
